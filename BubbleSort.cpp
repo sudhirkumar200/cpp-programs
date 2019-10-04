4095906aad2b9cb6f66d852121f2e20bc5add439
Saurav Kumar <saurav_1901ee54@iitp.ac.in>
2019-10-04 07:44:33 +0530

    Add files via upload

---
+++ BubbleSort.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace::std;
+const int size=40;
+int array[size],n;
+void bubble_sort()
+{    
+    int temp;
+    for (int i=n-1;i>0;i--)
+     {
+		for(int j=0;j<i;j++)
+			{
+				if(array[j]<array[j+1])
+					{
+						temp=array[j];
+						array[j]=array[j+1];
+						array[j+1]=temp;
+					}
+			}
+	 }
+
+}
+int main()
+{
+	system("cls");
+	cout<<"A program to Sort a 1-D array in Descending order\n";
+	cout<<"Enter number of elements of the array(max 40):\t";
+	cin>>n;
+	cout<<"Enter Array Elements:\n";
+	for(int i=0;i<n;i++)
+    cin>>array[i];
+    bubble_sort();
+    cout<<"Sorted array:\n";
+    for(int j=0;j<n;j++)
+    cout<<array[j]<<" ";
+    system("PAUSE");
+    return 0;
+}
+
---
+++ Lsearch.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace::std;
+const int size=40;
+int array[size],n;
+void Lsearch(int ele)
+{	int found=0,pos;
+	for(int i=0;i<n;i++)
+	{	if(ele==array[i])
+ 		{	found=1;
+  			pos=i;
+  			break;
+		}
+	}
+	if(found)
+	 cout<<"Element found at index:"<<pos<<",Position:"<<pos+1<<endl;
+	else
+	 cout<<"Sorry!!!Element could not be found.";
+}
+int main()
+{	int elem;
+	system("cls");
+	cout<<"Enter size of array(max 40):";
+	cin>>n;
+	cout<<"Enter Array Elements:\n";
+	for(int i=0;i<n;i++)
+	  cin>>array[i];
+	cout<<"Enter element to be searched for:";
+	cin>>elem;
+	Lsearch(elem);
+	return 0;
+}
+
